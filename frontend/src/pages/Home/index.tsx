a029fda18d58cd954cd43290cb761b79021185ba
Uillianleal <willianleal52@hotmail.com>
2022-07-19 12:03:40 -0300

    First commit

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+
+        </div>
+    );
+}
+
+export default Home;
