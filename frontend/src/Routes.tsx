0d11c36f85a687fb83c705fca1d5c686faadb264
Uillianleal <willianleal52@hotmail.com>
2022-07-21 11:11:27 -0300

    Projeto finalizado

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
-import CepSearch from 'pages/CepSearch';
+import GitSearch from 'pages/GitSearch';
 
 const Routes = () => (
   <BrowserRouter>
     <Navbar />
-    
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/gitsearch">
+        <GitSearch />
+      </Route>
+    </Switch>
   </BrowserRouter>
 );
 
